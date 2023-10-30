c12f8c6030d177904adb11a43dc33855aada1a71
voomp <bat7gamer7@gmail.com>
2023-10-29 20:42:05 -0400

    next/head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>RiskReady</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
